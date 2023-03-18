c4f5712b804f5f9fd84ca6ad9697d9eca982897d
Chika Ohaya <chika.ohaya@gmail.com>
2023-03-17 21:11:35 -0400

    update

--- foodapi.py
+++ foodapi.py
@@ -21,24 +21,32 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+client = None
+
 class RestaurantModel:
     db_name = "Foodiez"
     collection_name = "Restaurants"
 
     @staticmethod
     async def get_all_restaurants() -> List[dict]:
-        mongo_uri = os.environ.get("mongodb://localhost:27017/")
-        client = AsyncIOMotorClient(mongo_uri)
         db = client[RestaurantModel.db_name]
         collection = db[RestaurantModel.collection_name]
         cursor = collection.find({})
         restaurants = list(await cursor.to_list(length=None))
-
-        client.close() # close the connection to the MongoDB server after fetching the data
         return restaurants
 
+@app.on_event("startup")
+async def startup_event():
+    global client
+    mongo_uri = os.environ.get("mongodb://localhost:27017/")
+    client = AsyncIOMotorClient(mongo_uri)
 
-@app.get("/getRestaurants")
+@app.on_event("shutdown")
+async def shutdown_event():
+    global client
+    client.close()
+
+@app.get("/getRestaurant")  # Changed from /getRestaurants to /getRestaurant
 async def get_restaurants() -> List[dict]:
     try:
         restaurants = await RestaurantModel.get_all_restaurants()
@@ -48,15 +56,9 @@ async def get_restaurants() -> List[dict]:
         raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
 
     # Convert the response to a JSON-serializable format
-    # Convert the response to a JSON-serializable format and add CORS headers
     response = json.loads(json_util.dumps(restaurants))
     return {
         "status": "success",
         "data": response,
         "Access-Control-Allow-Origin": "*", # Update with your app's domain or '*' to allow all domains
     }
-
-
-
-
-
